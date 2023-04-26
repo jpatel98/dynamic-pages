43967e2e3cba315550be16e9fbe604bf0f6d0560
Jigar Patel <jpxtel98@outlook.com>
2023-04-25 21:32:16 -0400

    Create shell functions for script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function collectUserPreferences() {
+  // Collect user preferences from input fields
+}
+
+function constructQuery(preferences) {
+  // Construct the query based on user preferences
+}
+
+function callOpenAiApi(query) {
+  // Call the OpenAI API with the query and return a Promise with the response
+}
+
+function displayGeneratedWebPage(html, css) {
+  // Display the generated web page in the output container
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Add event listeners for buttons and input fields
+  // Example: document.getElementById('submit-button').addEventListener('click', onSubmitButtonClick);
+});
